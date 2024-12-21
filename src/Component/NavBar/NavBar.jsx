8d33d97ea088777ca62102354e4e7e48998d9ece
Mahmoud-Emad-111 <mahmoud.devops@gmail.com>
2024-12-21 06:45:01 +0200

    nav

--- src/Component/NavBar/NavBar.jsx
+++ src/Component/NavBar/NavBar.jsx
@@ -1,24 +1,27 @@
-import React from 'react'
-import logo from '../../assets/logooo.png'
-import { Link } from 'react-router-dom'
-import './NavBar.css';
-export default function NavBar() {
-  return (
-    <div className='NavBar container'>
-    <div className="logo">
-      <Link to='/'>        <img src={logo} alt="" />      </Link>
-    </div>
-    <div className='links'>
-        <Link to='/About'>About</Link>
-        <Link to='/Services'>Services</Link>
-        <Link to='/Projects'>Projects</Link>
-        <Link to='/Faqs'>faqs</Link>
-        <Link to='/Contact'>Contact</Link>
-    </div>
-    <div className='lang'>
-    <Link to='/Jop' target='_blank'>apply for job</Link>
-     <button>AR</button>
-    </div>
-    </div>
-  )
+import React from 'react';  
+import logo from '../../assets/logooo.png';  
+import { Link } from 'react-router-dom';  
+import './NavBar.css';  
+  
+export default function NavBar() {  
+  return (  
+   <nav className="NavBar container">  
+    <div className="logo">  
+      <Link to="/">  
+       <img src={logo} alt="Logo" />  
+      </Link>  
+    </div>  
+    <div className="links">  
+      <Link to="/About">About</Link>  
+      <Link to="/Services">Services</Link>  
+      <Link to="/Projects">Projects</Link>  
+      <Link to="/Faqs">FAQs</Link>  
+      <Link to="/Contact">Contact</Link>  
+    </div>  
+    <div className="lang">  
+      <Link to="/Jop" target="_blank">Apply for Job</Link>  
+      <button>AR</button>  
+    </div>  
+   </nav>  
+  );  
 }
